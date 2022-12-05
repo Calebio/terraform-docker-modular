b9797c295beccf198ad49c3baace423819a9e43e
Caleb <kalebiotezz@yahoo.com>
2022-12-05 12:48:37 +0000

    init frst

--- container/main.tf
+++ container/main.tf
@@ -5,9 +5,6 @@ resource "random_string" "string_rand" {
   special = false
 }
 
-
-# Create the container
-
 resource "docker_container" "app_container" {
   count = var.count_in
   name  = join("-", [var.name_in, terraform.workspace, random_string.string_rand[count.index].result])
@@ -16,40 +13,26 @@ resource "docker_container" "app_container" {
     internal = var.int_port_in
     external = var.ext_port_in[count.index]
   }
-
   dynamic "volumes" {
     for_each = var.volumes_in
-    content{
+    content {
       container_path = volumes.value["container_path_each"]
-    volume_name    = docker_volume.container_volume[volumes.key].name
+      volume_name    = module.volume[count.index].volume_output[volumes.key]
     }
   }
   provisioner "local-exec" {
-    when       = destroy
-    command    = "echo ${self.name}: ${self.ip_address}:${join("", [for x in self.ports[*]["external"] : x])} >> containers.txt"
-    on_failure = fail
+    command = "echo ${self.name}: ${self.ip_address}:${join("", [for x in self.ports[*]["external"] : x])} >> test.txt"
   }
   provisioner "local-exec" {
-    when       = destroy
-    command    = "rm -f container.txt"
-    on_failure = fail
+    when    = destroy
+    command = "rm -f test.txt"
   }
 }
 
-resource "docker_volume" "container_volume" {
-  count = length(var.volumes_in)
-  name  = "${var.name_in}-${count.index}-volume"
-  lifecycle {
-    prevent_destroy = false
-  }
-  provisioner "local-exec" {
-    when       = destroy
-    command    = "mkdir ${path.cwd}/../backup"
-    on_failure = continue
-  }
-  provisioner "local-exec" {
-    when       = destroy
-    command    = "sudo tar -czvf ${path.cwd}/../backup/${self.name}.tar.gz ${self.mountpoint}/"
-    on_failure = fail
-  }
+module "volume" {
+  source       = "./volume"
+  count        = var.count_in
+  volume_count = length(var.volumes_in)
+  volume_name  = "${var.name_in}-${terraform.workspace}-${random_string.string_rand[count.index].result}-volume"
 }
+
--- terraform.tfvars
+++ terraform.tfvars
@@ -9,7 +9,7 @@ ext_port = {
     prod = [8086]
   }
   grafana = {
-    dev  = [3100]
+    dev  = [3100, 3101]
     prod = [3000]
   }
 }
